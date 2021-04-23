74ee4871417096f2500e948a05be117dc519d4f6
Yanyao Wang <wangyanyao@msn.com>
2021-04-23 08:14:39 -0700

    Update check.py

--- pytorch-release/check.py
+++ pytorch-release/check.py
@@ -2,18 +2,18 @@
 
 try:
     import torch
+    print("pytorch is installed...")
 except ImportError as error:
     print("torch is NOT found...")
-print("pytorch is installed...")
 
 try:
     import apex
+    print("apex is installed...")
 except ImportError as error:
     print("apex is NOT found...")
-print("apex is installed...")
 
 try:
     import torchvision
+    print("torchvision is installed...")
 except ImportError as error:
     print("torchvision is NOT found...")
-print("torchvision is installed...")
