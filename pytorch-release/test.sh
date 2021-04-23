83b7d32c6da912fcab6afbbccdc83f634a204caa
Yanyao Wang <wangyanyao@msn.com>
2021-04-23 09:11:52 -0700

    Update test.sh

--- pytorch-release/test.sh
+++ pytorch-release/test.sh
@@ -2,8 +2,17 @@ CURRENT_FOLDER=/var/lib/jenkins
 
 CURT_DOCKER_OS=$(. /etc/os-release && echo "$ID")
 CURT_DOCKER_OS_VER=$(. /etc/os-release && echo "$VERSION_ID")
-FW_NAME=$(echo $BUILD_ENVIRONMENT | cut -d "-" -f 1)
-FW_BRANCH=$(cd pytorch/; git branch; cd ..)
+#FW_NAME=$(echo $BUILD_ENVIRONMENT | cut -d "-" -f 1)
+TEMP=$(ls)
+if [[ "$TEMP" =~ ^caffe2 ]];
+then
+    FW_NAME=caffe2
+fi
+if [[ "$TEMP" =~ ^pytorch ]];
+then
+    FW_NAME=pytorch
+fi
+FW_BRANCH=$(cd $CURRENT_FOLDER/$FW_NAME; git branch; cd ..)
 
 if [ "$CURT_DOCKER_OS" = "ubuntu" ];
 then
@@ -12,6 +21,20 @@ then
     echo "=========================================================="
     echo "Current OS: $CURT_DOCKER_OS    Version: $CURT_DOCKER_OS_VER"
     rocm_pkg_version=$(dpkg -s rocm-dev | grep "Version")
+    echo "Current ROCm Version: $rocm_pkg_version"
+    echo "Current FW Name: $FW_NAME"
+    echo "Current FW Branch: $FW_BRANCH"
+    echo "=========================================================="
+fi
+
+if [ "$CURT_DOCKER_OS" = "centos" ];
+then
+    echo "=========================================================="
+    echo "GENERAL CHECK"
+    echo "=========================================================="
+    echo "Current OS: $CURT_DOCKER_OS    Version: $CURT_DOCKER_OS_VER"
+
+    rocm_pkg_version=$(rpm -q rocm-dev)
     echo "Current ROCm Version: $rocm_pkg_version"
     echo "Current FW Name: $FW_NAME"
     echo "Current FW Branch: $FW_BRANCH"
