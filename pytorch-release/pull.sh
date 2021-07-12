c461ea68d3fa1e090d6957c4e32898775eaf597c
Jenkins <jenkins-compute@amd.com>
2021-07-12 06:03:02 +0000

    ssh19 add gpu arch check. apex and vision version check

--- pytorch-release/pull.sh
+++ pytorch-release/pull.sh
@@ -1,9 +1,10 @@
 components=(
-compute-artifactory.amd.com:5000/rocm-plus-docker/framework/compute-rocm-rel-4.2:21_centos7.5.1804_py3.6_caffe2_rocm4.2_internal_testing_30a6f3d_152
-compute-artifactory.amd.com:5000/rocm-plus-docker/framework/compute-rocm-rel-4.2:21_ubuntu18.04_py3.6_caffe2_rocm4.2_internal_testing_30a6f3d_152
-compute-artifactory.amd.com:5000/rocm-plus-docker/framework/compute-rocm-rel-4.2:21_centos7.5.1804_py3.6_pytorch_rocm4.2_internal_testing_30a6f3d_152
-compute-artifactory.amd.com:5000/rocm-plus-docker/framework/compute-rocm-rel-4.2:21_ubuntu18.04_py3.6_pytorch_rocm4.2_internal_testing_30a6f3d_152
+compute-artifactory.amd.com:5000/rocm-plus-docker/framework/release-public:rocm4.1_centos7.5.1804_py3.6_caffe2_157
+compute-artifactory.amd.com:5000/rocm-plus-docker/framework/release-public:rocm4.1_centos7.5.1804_py3.6_pytorch_157
+compute-artifactory.amd.com:5000/rocm-plus-docker/framework/release-public:rocm4.1_ubuntu18.04_py3.6_caffe2_157
+compute-artifactory.amd.com:5000/rocm-plus-docker/framework/release-public:rocm4.1_ubuntu18.04_py3.6_pytorch_157
 )
+
 
 #echo "${components[0]}"
 #echo "${components[1]}"
---
+++ pytorch-release/push.sh
@@ -0,0 +1,10 @@
+docker push rocm/pytorch:rocm4.2_ubuntu18.04_py3.6_pytorch_1.8.0
+docker push rocm/pytorch:rocm4.2_ubuntu18.04_py3.6_pytorch_1.7.0
+
+docker push rocm/pytorch:rocm4.2_ubuntu18.04_py3.6_pytorch
+docker push rocm/pytorch:rocm4.2_ubuntu18.04_py3.6_caffe2
+docker push rocm/pytorch:rocm4.2_centos7_py3.6_pytorch
+docker push rocm/pytorch:rocm4.2_centos7_py3.6_caffe2
+
+docker push rocm/pytorch:latest
+
