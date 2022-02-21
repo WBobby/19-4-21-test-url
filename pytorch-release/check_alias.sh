6a96ad63a6e0eddfd9b050395530b6e59b647d40
Jenkins <jenkins-compute@amd.com>
2022-02-21 18:03:33 -0500

    hope to add check tag

---
+++ pytorch-release/check_alias.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+declare ALIAS_DICT=(
+    [rocm/pytorch:rocm5.0.1_ubuntu18.04_py3.7_pytorch_1.10.0]="rocm/pytorch:latest"
+    [rocm/pytorch:rocm5.0.1_centos7_py3.7_pytorch_staging]="rocm/pytorch:latest-centos7"
+    [rocm/pytorch:rocm5.0.1_ubuntu18.04_py3.7_pytorch_1.10.0_base]="rocm/pytorch:latest-base"
+)
+
